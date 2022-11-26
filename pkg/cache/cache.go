22a762495e016c31775e0f804b43a73b1ed4df31
GoldbergDL <dmitrijj.goldberg@vseinstrumenti.ru>
2022-11-26 18:03:13 +0300

    Implemented lru cache

---
+++ pkg/cache/cache.go
@@ -0,0 +1,65 @@
+package cache
+
+type Key string
+
+type Cache interface {
+	Set(key Key, value interface{}) bool
+	Get(key Key) (interface{}, bool)
+	Clear()
+}
+
+type lruCache struct {
+	capacity int
+	queue    List
+	items    map[Key]*ListItem
+}
+
+type cacheItem struct {
+	key   Key
+	value interface{}
+}
+
+func (cache *lruCache) Set(key Key, value interface{}) bool {
+	newCacheItem := &cacheItem{key, value}
+
+	if queueItem, ok := cache.items[newCacheItem.key]; ok {
+		queueItem.Value = newCacheItem
+		cache.queue.MoveToFront(queueItem)
+
+		return ok
+	}
+
+	if cache.queue.Len() == cache.capacity {
+		toRemove := cache.queue.Back()
+		delete(cache.items, toRemove.Value.(*cacheItem).key)
+		cache.queue.Remove(toRemove)
+	}
+
+	cache.queue.PushFront(newCacheItem)
+	cache.items[newCacheItem.key] = cache.queue.Front()
+
+	return false
+}
+
+func (cache *lruCache) Get(key Key) (interface{}, bool) {
+	if queueItem, ok := cache.items[key]; ok {
+		cache.queue.MoveToFront(queueItem)
+
+		return queueItem.Value.(*cacheItem).value, ok
+	}
+
+	return nil, false
+}
+
+func (cache *lruCache) Clear() {
+	cache.queue = NewList()
+	cache.items = make(map[Key]*ListItem, cache.capacity)
+}
+
+func NewCache(capacity int) Cache {
+	return &lruCache{
+		capacity: capacity,
+		queue:    NewList(),
+		items:    make(map[Key]*ListItem, capacity),
+	}
+}
---
+++ pkg/cache/cache_test.go
@@ -0,0 +1,164 @@
+package cache
+
+import (
+	"math/rand"
+	"strconv"
+	"sync"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestCache(t *testing.T) {
+	t.Run("empty cache", func(t *testing.T) {
+		c := NewCache(10)
+
+		_, ok := c.Get("aaa")
+		require.False(t, ok)
+
+		_, ok = c.Get("bbb")
+		require.False(t, ok)
+	})
+
+	t.Run("simple", func(t *testing.T) {
+		c := NewCache(5)
+
+		wasInCache := c.Set("aaa", 100)
+		require.False(t, wasInCache)
+
+		wasInCache = c.Set("bbb", 200)
+		require.False(t, wasInCache)
+
+		val, ok := c.Get("aaa")
+		require.True(t, ok)
+		require.Equal(t, 100, val)
+
+		val, ok = c.Get("bbb")
+		require.True(t, ok)
+		require.Equal(t, 200, val)
+
+		wasInCache = c.Set("aaa", 300)
+		require.True(t, wasInCache)
+
+		val, ok = c.Get("aaa")
+		require.True(t, ok)
+		require.Equal(t, 300, val)
+
+		val, ok = c.Get("ccc")
+		require.False(t, ok)
+		require.Nil(t, val)
+	})
+
+	t.Run("purge logic by queue size", func(t *testing.T) {
+		c := NewCache(3)
+
+		wasInCache := c.Set("aaa", 100) // [aaa]
+		require.False(t, wasInCache)
+
+		wasInCache = c.Set("bbb", 200) // [bbb, aaa]
+		require.False(t, wasInCache)
+
+		wasInCache = c.Set("ccc", 300) // [ccc, bbb, aaa]
+		require.False(t, wasInCache)
+
+		wasInCache = c.Set("ddd", 400) // purge aaa -> [ddd, ccc, bbb]
+		require.False(t, wasInCache)
+
+		val, ok := c.Get("bbb")
+		require.True(t, ok)
+		require.Equal(t, 200, val)
+
+		val, ok = c.Get("ccc")
+		require.True(t, ok)
+		require.Equal(t, 300, val)
+
+		val, ok = c.Get("ddd")
+		require.True(t, ok)
+		require.Equal(t, 400, val)
+
+		val, ok = c.Get("aaa")
+		require.False(t, ok)
+		require.Nil(t, val)
+	})
+
+	t.Run("purge logic by queue frequency", func(t *testing.T) {
+		c := NewCache(3)
+
+		wasInCache := c.Set("aaa", 100) // [aaa]
+		require.False(t, wasInCache)
+
+		wasInCache = c.Set("bbb", 200) // [bbb, aaa]
+		require.False(t, wasInCache)
+
+		wasInCache = c.Set("ccc", 300) // [ccc, bbb, aaa]
+		require.False(t, wasInCache)
+
+		wasInCache = c.Set("aaa", 500) // [aaa, ccc, bbb]
+		require.True(t, wasInCache)
+
+		val, ok := c.Get("ccc") // [ccc, aaa, bbb]
+		require.True(t, ok)
+		require.Equal(t, 300, val)
+
+		val, ok = c.Get("aaa") // [aaa, ccc, bbb]
+		require.True(t, ok)
+		require.Equal(t, 500, val)
+
+		wasInCache = c.Set("ddd", 1000) // purge bbb -> // [ddd, aaa, ccc]
+		require.False(t, wasInCache)
+
+		val, ok = c.Get("bbb")
+		require.False(t, ok)
+		require.Nil(t, val)
+
+		wasInCache = c.Set("ppp", 0) // purge ccc -> // [ppp, ddd, aaa]
+		require.False(t, wasInCache)
+
+		val, ok = c.Get("ccc")
+		require.False(t, ok)
+		require.Nil(t, val)
+	})
+
+	t.Run("clear", func(t *testing.T) {
+		c := NewCache(5)
+
+		c.Set("aaa", 5)
+		c.Set("bbb", 7)
+
+		val, ok := c.Get("bbb")
+		require.True(t, ok)
+		require.Equal(t, 7, val)
+
+		c.Clear()
+
+		_, ok = c.Get("aaa")
+		require.False(t, ok)
+
+		_, ok = c.Get("bbb")
+		require.False(t, ok)
+	})
+}
+
+func TestCacheMultithreading(t *testing.T) {
+	t.Skip() // Remove me if task with asterisk completed.
+
+	c := NewCache(10)
+	wg := &sync.WaitGroup{}
+	wg.Add(2)
+
+	go func() {
+		defer wg.Done()
+		for i := 0; i < 1_000_000; i++ {
+			c.Set(Key(strconv.Itoa(i)), i)
+		}
+	}()
+
+	go func() {
+		defer wg.Done()
+		for i := 0; i < 1_000_000; i++ {
+			c.Get(Key(strconv.Itoa(rand.Intn(1_000_000))))
+		}
+	}()
+
+	wg.Wait()
+}
